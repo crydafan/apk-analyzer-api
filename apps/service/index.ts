8470290265a7bb9145ffa99f7907bce96e0b48ba
Leonardo <leonardo.estacio.h@gmail.com>
2025-12-21 01:52:49 -0500

    feat: service now fetches jobs

--- apps/service/index.ts
+++ apps/service/index.ts
@@ -1,11 +1,36 @@
-import { Elysia } from "elysia";
-import { db, schema } from "@common/db";
+import { Elysia, status, t } from "elysia";
+import { db, schema, operator } from "@common/db";
 
 const app = new Elysia()
-  .get("/", () => {})
+  .get(
+    "/:id",
+    async ({ params }) => {
+      const [job] = await db
+        .select()
+        .from(schema.jobsTable)
+        .where(operator.eq(schema.jobsTable.id, params.id));
+      if (!job) {
+        return status(404, { success: false, message: "Job not found" });
+      }
+      return {
+        success: true,
+        message: "Job retrieved successfully",
+        data: job,
+      };
+    },
+    {
+      params: t.Object({
+        id: t.String(),
+      }),
+    }
+  )
   .post("/", async () => {
     const [job] = await db.insert(schema.jobsTable).values({}).returning();
-    return job;
+    return status(201, {
+      success: true,
+      message: "Job created",
+      data: job,
+    });
   })
   .listen(3000);
 
--- common/db/index.ts
+++ common/db/index.ts
@@ -1,6 +1,7 @@
 import { drizzle } from "drizzle-orm/bun-sql";
 import * as schema from "./db/schema";
+import * as operator from "./db/operator";
 
 const db = drizzle(process.env.DATABASE_PUBLIC_URL!, { schema });
 
-export { db, schema };
+export { db, schema, operator };
