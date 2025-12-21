3d914f4dcf0d0abaff5a63e09ffd20f0bf077d63
Leonardo <leonardo.estacio.h@gmail.com>
2025-12-21 13:18:34 -0500

    feat: simulate processing task and report status

--- apps/agent/index.ts
+++ apps/agent/index.ts
@@ -1,6 +1,55 @@
-import { sub, Ack } from "@common/rabbit";
+import { sub, Ack, Drop } from "@common/rabbit";
+import { db, schema, operator } from "@common/db";
+
+type MessageBody = {
+  id: string; // Job id
+  key: string; // Storage key
+};
 
 const consumer = sub("jobs", async (msg) => {
+  const body: MessageBody = msg.body;
+
+  const [job] = await db
+    .select()
+    .from(schema.jobsTable)
+    .where(operator.eq(schema.jobsTable.id, body.id))
+    .limit(1);
+
+  if (!job) {
+    // No job with such id found, to whom are we supposed to process this?
+    console.log(`⚙️ Job with id ${body.id} not found, dropping message.`);
+    return Drop;
+  }
+
+  console.log(`⚙️ Processing job with id ${body.id}`);
+
+  job.status = "in_progress";
+  job.updatedAt = new Date();
+
+  await db
+    .update(schema.jobsTable)
+    .set({
+      status: job.status,
+      updatedAt: job.updatedAt,
+    })
+    .where(operator.eq(schema.jobsTable.id, body.id));
+
+  // Simulate a 3 second task
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  job.status = "completed";
+  job.updatedAt = new Date();
+
+  await db
+    .update(schema.jobsTable)
+    .set({
+      status: job.status,
+      updatedAt: job.updatedAt,
+    })
+    .where(operator.eq(schema.jobsTable.id, body.id));
+
+  console.log(`⚙️ Completed job with id ${body.id}`);
+
   return Ack;
 });
 consumer.start();
