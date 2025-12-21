49a49e09579bd49cbf070e82de67485f9a78fd6a
Leonardo <leonardo.estacio.h@gmail.com>
2025-12-21 01:44:07 -0500

    feat: post now creates jobs

--- apps/service/index.ts
+++ apps/service/index.ts
@@ -1,6 +1,13 @@
 import { Elysia } from "elysia";
+import { db, schema } from "@common/db";
 
-const app = new Elysia().get("/", () => "Hello World!").listen(3000);
+const app = new Elysia()
+  .get("/", () => {})
+  .post("/", async () => {
+    const [job] = await db.insert(schema.jobsTable).values({}).returning();
+    return job;
+  })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
